09c1ed9c875bb8ded8d5b9b454f1658c3a044bf7
PoojithaPradeep <70422047+PoojithaPradeep@users.noreply.github.com>
2020-08-28 23:52:27 -0400

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,19 @@
+strings
+a= 'apple is a fruit'
+a.lower()
+a.upper()
+a.title()
+a.islower()
+a.isupper()
+a.istitle()
+a.startswith('a')
+a.endswith('e')
+a.swapcase()
+a.replace('a','b')
+a.split()
+' '.join(c)
+range
+b = list(range(1,11))
+
+for i in b[::-1]:
+    print(i, end =',')
